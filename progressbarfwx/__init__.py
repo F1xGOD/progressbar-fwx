b3bca1f16327440d7c35789b7eb57db68ba9af08
F1xGOD <105874129+F1xGOD@users.noreply.github.com>
2024-10-14 16:53:34 -0700

    2024

--- progressbarfwx/__init__.py
+++ progressbarfwx/__init__.py
@@ -1,5 +1,5 @@
 from .main import *
 def setprecent(title: str, amo, oof):
     progress.setprecent(title,amo,oof)
-def start():
-    progress.start()
+def start(title: str):
+    progress.start(title)
